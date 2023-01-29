ea4625c74bd5dd371f8efe03ed7f2ba5c9da9fe7
KochkinAnna <kochkinaanichka@gmail.com>
2023-01-29 22:20:21 +0200

    todos & albums & comments & users & notfound

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -3,6 +3,7 @@ import css from './NotFoundPage.module.css'
 const NotFoundPage = () => {
     return (
         <div className={css.NotFoundPage}>
+            <img src={'https://miro.medium.com/max/1200/1*EQisBuMOijQT8woW0Jn6pA.jpeg'} alt={'NotFound'}/>
         </div>
     );
 };
