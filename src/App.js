ca2236d3dc135842ae876c34e17cd3a9220e200e
KochkinAnna <kochkinaanichka@gmail.com>
2023-02-08 16:21:30 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import {Cars, Form} from "./components";
+import {useSelector} from "react-redux";
+
 const App = () => {
+    const {loading} = useSelector(state => state.cars);
     return (
         <div>
-            App
+            <Form/>
+            <hr/>
+            {loading&& <h2>loading...</h2>}
+            <Cars/>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
 
 import './index.css';
 import {App} from './App';
+import {setUpStore} from "./redux";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = setUpStore();
+
 root.render(
+    <Provider store={store}>
     <App/>
+    </Provider>
 );
 
